0dbbbf72bfd69de1637c31d393ebc07b1a87fd56
Maxly Garcia <msgarcia@LAPTOP-OTJ57VR2.localdomain>
2021-04-27 00:14:46 -0400

    Add routes. Set up connection to DB

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
 
+require('dotenv/config');
+
+//Import Routes
+const doctorRoute = require('./routes/doctors');
+const userRoute = require('./routes/users');
+
+//MIDDLEWARES
+app.use('/doctors', doctorRoute);
+app.use('/users', userRoute);
+
+
+//ROUTES
 app.get('/', (req, res) => {
     res.send('Home Page');
 });
+
+//Connect to DB
+//{useNewUrlParser: true}
+mongoose.connect(process.env.DB_CONNECTION,
+{ useUnifiedTopology: true, useNewUrlParser: true }, () => 
+    console.log("Connected to db"),
+);
+
 
 app.listen(4001);
