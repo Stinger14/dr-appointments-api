e68a228f87918ac0dbb2f32d5d165e2865c4ef1b
Maxly Garcia <maxgarcia6@gmail.com>
2021-04-27 00:18:02 -0400

    Add models. Save creds to DB on a json file

---
+++ models/Doctor.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const DoctorSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    experience: Float,
+    specialty: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Doctors', DoctorSchema);
