27e21567439c5fadfb1464df4d941ef3dc24ebef
Maxly Garcia <maxgarcia6@gmail.com>
2021-04-28 00:04:36 -0400

    Improve routes

--- routes/doctors.js
+++ routes/doctors.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Handling GET requests to /doctors"
+    });
 });
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: "Doctor was created"
+    });
+});
+
+// ':' dynamic param in express
+router.get('/:doctorId', (req, res, next) => {
+    if (id == 'special') {
+        res.status(200).json({
+            message: 'You discovered a special ID',
+            doctorId : req.params.orderId
+        });
+    } 
+});
+
+router.patch('/:doctorId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Doctor updated'
+    });
+});
+
+router.delete('/:doctorId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Doctor deleted'
+    });
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+const User = require('../models/User');
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Handling GET requests to /users"
+    });
+});
+
+router.post('/', (req, res, next) => {
+    // const user = {
+    //     name: req.body.name,
+    //     location: req.body.location,
+    //     reviews: req.body.reviews
+    // };
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        location: req.body.location,
+        reviews: req.body.reviews
+    });
+    user.save().then(res => {
+        console.log(res);
+    })
+    .catch(err => console.log(err))
+    res.status(201).json({
+        message: "User created",
+        createdUser: user
+    });
+});
+
+router.get('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    if (id == 'special') {
+        res.status(200).json({
+            message: 'You discovered a special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:userId', (req, res, next) => {
+    res.status(200).json({
+        message: 'User updated'
+    });
+});
+
+router.delete('/:userId', (req, res, next) => {
+    res.status(200).json({
+        message: 'User deleted'
+    });
 });
 
 module.exports = router;
