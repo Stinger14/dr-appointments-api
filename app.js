93475985293863c898873479146e43d8f38021cd
Maxly Garcia <maxgarcia6@gmail.com>
2021-04-28 00:06:31 -0400

    Change API entry point now server.js, add middlewares, enable CORS handling

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 const express = require('express');
+const logger = require('morgan');       // loggin
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv');
+// const bodyPardser = require('body-parser');  // depricated
 
-require('dotenv/config');
+// Import routes
+const userRoutes = require('./routes/users');
+const doctorRoutes = require('./routes/doctors');
 
-//Import Routes
-const doctorRoute = require('./routes/doctors');
-const userRoute = require('./routes/users');
+// Middlewares
+app.use(express.json());
+app.use(logger('dev'));
 
-//MIDDLEWARES
-app.use('/doctors', doctorRoute);
-app.use('/users', userRoute);
+//Handle CORS errors
+app.use((req, res, next) => {
+    //? add headers to response
+    // * = allow anything
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With',
+                'Content-Type', 'Accept', 'Authorization');
 
-
-//ROUTES
-app.get('/', (req, res) => {
-    res.send('Home Page');
+    //? check if incoming requet method == OPTIONS
+    //? Browser always sends OPTIONS method prior to response
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
+        return res.status(200).json({});
+    }
+    next();     //let other routes takeover if requets gets blocked
 });
 
-//Connect to DB
-//{useNewUrlParser: true}
-mongoose.connect(process.env.DB_CONNECTION,
-{ useUnifiedTopology: true, useNewUrlParser: true }, () => 
-    console.log("Connected to db"),
-);
+// Endpoints
+app.use('/users', userRoutes);
+app.use('/doctors', doctorRoutes);
 
+// Connect to DB
+mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_PW + '@apishopcluster.p2bh3.mongodb.net/doctors-appointment?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 app.listen(4001);
